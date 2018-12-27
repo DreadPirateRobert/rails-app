b35303852ae15a376fc2cf9ea3718576d903de98
DreadPirateRobert <pratik.sal13@gmail.com>
2018-12-27 17:08:09 +0530

    before signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
   resources :microposts
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/home',    to: 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
