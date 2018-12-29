729410146cc2b97be02814b1866ad82b076b4abb
DreadPirateRobert <pratik.sal13@gmail.com>
2018-12-29 13:35:24 +0530

    added signup,gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
+  has_secure_password
   #validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
   #validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
 
