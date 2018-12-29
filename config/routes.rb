0aee9a6982d8e4062dd4a1dd50bf32a72c3a3a76
DreadPirateRobert <pratik.sal13@gmail.com>
2018-12-29 16:45:55 +0530

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # get 'static_pages/home'
   # get 'static_pages/help'
   resources :microposts
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/home',    to: 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
